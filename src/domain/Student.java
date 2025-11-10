c4a185c30cb54f8ad2352f8ee47eda63656ac1ad
wlsgud seo <fcipras8694@naver.com>
2025-11-10 15:57:32 +0900

    student class 수정

---
+++ src/domain/Student.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Student {
+    int id;
+    String name;
+
+    public void Signup(int id, String name ){
+        this.id = id;
+        this.name = name;
+    };
+
+    public String GetName() {return name;}
+    public int getstudentId(){return id;}
+}
