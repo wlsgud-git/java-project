5ad0c693949b9f060af2e0182fc7fde771b7c35d
wlsgud seo <fcipras8694@naver.com>
2025-11-10 15:40:18 +0900

    first commit

---
+++ src/MainTester.java
@@ -0,0 +1,8 @@
+// import domain.Character;
+// import domain.SuperCharacter;
+// import controller.CharacterController;
+
+public class MainTester {
+    public static void main(String[] args) {
+    }
+}
